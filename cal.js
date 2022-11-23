dee6d88c0db0989918c6f0be5b26a1607eada6cd
Akshay Rajendran <akshayaromal9@gmail.com>
2022-11-23 08:58:52 +0530

    completed calculator

--- cal.js
+++ cal.js
@@ -1,7 +1,67 @@
-let main=document.querySelector(".numb")
-let screen=document.querySelector("input")
+let main = document.querySelector(".numb")
+let button = document.querySelector("button")
+let screen = document.querySelector("input")
+let first = 0
+let second = 0
+let operator
 
-main.addEventListener("click",(e)=>{
-    console.log(e.target.innerHTML);
-    screen.value= screen.value+e.target.innerHTML
+main.addEventListener("click", (e) => {
+    targ = e.target
+    console.log(targ.style.background )
+    targ.style.background = "red"
+    // console.log(e.target.classList)
+    if (targ.classList.contains("numbtn")) {
+        screen.value = screen.value + e.target.innerHTML
+    }
+    
+    else {
+        switch (targ.innerHTML) {
+            case '+':
+            case '-':
+            case '/':
+            case '*':
+                first = parseFloat(screen.value)
+                operator = targ.innerHTML
+                screen.value = ""
+                break;
+            case '=':
+                console.log(first, operator);
+                second=parseFloat(screen.value)
+                printAnswer(first,second,operator)
+                break;
+            case'c':
+                screen.value=""    
+        }
+
+
+    }
+    setInterval(()=>{
+        targ.style.background = "buttonface"
+        targ.style.border="1px solid black"
+    
+    },500)
+
+    
 })
+
+
+
+function printAnswer(first,second,operator){
+    let result
+    switch(operator){
+        case'+':
+          result=first+second
+          break;
+        case'-':
+           result=first-second
+           break;
+        case'/':
+           result=first/second
+           break;
+        case'*':
+           result=first*second
+           break;       
+
+    }
+   screen.value=result
+}
